372ce57bf38c36f8455a757d8ebb585f6dbc0016
SAHIL NAZARE <sahilnazare31@gmail.com>
2023-04-01 12:51:51 +0530

    introduced profile sec

--- GUI/gui/main.py
+++ GUI/gui/main.py
@@ -29,14 +29,61 @@ class MainWindow(QMainWindow):
         self.ui.upload_videos_FL.addRow(QPushButton("BROWSE VIDEOS", clicked = lambda: self.upload_videos()))
         self.ui.add_camera_FL.addRow(QPushButton("ADD CAMERA", clicked = lambda: self.add_camera()))
         self.ui.save_sett_btn.clicked.connect(self.save_settings)
-
-        self.update_list('images')
-        self.update_list('videos')
+        self.ui.images_LW.itemClicked.connect(self.images_clicked_LW)
+        self.ui.videos_LW.itemClicked.connect(self.videos_clicked_LW)
              
-        # self.queue = Queue(maxsize=1000)
-        # self.start_video_thread()
-        # self.start_recog_thread()
+        self.queue = Queue(maxsize=1000)
+        self.start_video_thread()
+        self.start_recog_thread()
   
+    def images_clicked_LW(self, item):
+
+        if item.text() == '----NO DATA TO SHOW----':
+            return
+
+        folder = os.path.join(os.path.dirname(__file__), 'uploaded_images', item.text())
+        filename = os.path.join(folder, os.listdir(folder)[0])
+
+        self.ui.image_lbl.setPixmap(QPixmap(filename))
+        self.ui.profile_sec_lbl.show()
+        self.ui.profile_sec.show()
+        self.ui.name.setText(item.text())
+
+        # get encoding generated time here
+        self.ui.enc_time.setText('--time--')
+
+        # link delete encoding method here
+        # self.ui.del_btn.clicked.connect()
+
+    def videos_clicked_LW(self, item):
+
+        if item.text() == '----NO DATA TO SHOW----':
+            return
+
+        folder = os.path.join(os.path.dirname(__file__), 'uploaded_videos', item.text())
+        filename = os.path.join(folder, os.listdir(folder)[0])
+
+        # extracting first frame
+        os.chdir(folder)
+        if 'first_frame.jpg' not in os.listdir(folder):
+            cap = cv2.VideoCapture(filename)
+            success, image = cap.read()
+            if success:
+                cv2.imwrite('first_frame.jpg', image)
+
+        self.ui.image_lbl.setPixmap(QPixmap('first_frame.jpg'))
+        self.ui.profile_sec_lbl.show()
+        self.ui.profile_sec.show()
+        self.ui.name.setText(item.text())
+
+        # get encoding generated time here
+        self.ui.enc_time.setText('--time--')
+
+        # link delete encoding method here
+        # self.ui.del_btn.clicked.connect()
+
+
+        
  
     def start_video_thread(self):
         self.detection_model_name = self.get_detection_model()
@@ -105,7 +152,7 @@ class MainWindow(QMainWindow):
             self.change_recog_model()
 
     def upload_images(self):
-        # ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+        
         path = os.path.join(os.path.dirname(__file__), 'uploaded_images', self.ui.upload_images_text.text())
         try: os.mkdir(path)
         except OSError: pass
@@ -135,21 +182,21 @@ class MainWindow(QMainWindow):
         if not os.path.isdir(path):
             os.makedirs(path)
         vid_paths, _ = QFileDialog.getOpenFileNames(None, "UPLOAD VIDEOS", os.path.dirname(__file__), "Videos (*.mp4)")
-        i = len(os.listdir(path))
+        
+        file_list = os.listdir(path)
+        if 'first_frame.jpg' in file_list:
+            file_list.remove('first_frame.jpg')
+        i = len(file_list)
 
         if vid_paths:
             for vid_path in vid_paths:
                 fname = name+"_"+str(i)+"."+str(vid_path.split('.')[-1])
                 path = os.path.join(path, fname)
-                # print(path)
-                # print(vid_path)
                 shutil.copy(vid_path, path)
-                # print(path)
                 t1 = Thread(target=make_enc,args=(path,name))
                 t1.start()
                 t1.join()
                 
-
             self.start_recog_thread()
           
             QMessageBox.information(self.ui.upload_videos_page, 'Success', 'Videos uploaded successfully!')
@@ -166,23 +213,52 @@ class MainWindow(QMainWindow):
         self.ui.add_camera_text.clear()
 
 
+
     # -----don't change this-----
     def update_list(self, page):
+
         if page == 'images':
             path = os.path.join(os.path.dirname(__file__), 'uploaded_images')
             self.ui.images_list_widget.clear()
         else:
-            path = os.path.join(os.path.dirname(__file__), f'uploaded_videos')
+            path = os.path.join(os.path.dirname(__file__), 'uploaded_videos')
             self.ui.videos_list_widget.clear()
         if not os.path.isdir(path):
             os.mkdir(path)
+        if len(os.listdir(path)) == 0:
+            if page == 'images': QListWidgetItem('----NO DATA TO SHOW----', self.ui.images_list_widget)
+            else: QListWidgetItem('----NO DATA TO SHOW----', self.ui.videos_list_widget)
+            return
         for i in os.listdir(path):
             inner_dir_path = os.path.join(path, i)
             if not os.path.isfile(inner_dir_path):
-                for ele_path in os.listdir(inner_dir_path):
+                file_list = os.listdir(inner_dir_path)
+                if 'first_frame.jpg' in file_list:
+                    file_list.remove('first_frame.jpg')
+                for ele_path in file_list:
                     if page == 'images': QListWidgetItem(path+'\\'+i+'\\'+ele_path, self.ui.images_list_widget)
                     else: QListWidgetItem(path+'\\'+i+'\\'+ele_path, self.ui.videos_list_widget)
 
+    def show_data_list(self, page):
+        
+        if page == 'images':
+            path = os.path.join(os.path.dirname(__file__), 'uploaded_images')
+            self.ui.images_LW.clear()
+        else:
+            path = os.path.join(os.path.dirname(__file__), 'uploaded_videos')
+            self.ui.videos_LW.clear()
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        if len(os.listdir(path))==0:
+            if page == 'images': QListWidgetItem('----NO DATA TO SHOW----', self.ui.images_LW)
+            else: QListWidgetItem('----NO DATA TO SHOW----', self.ui.videos_LW)
+            return
+        for i in os.listdir(path):
+            if page == 'images': 
+                self.ui.images_LW.addItem(i)
+            else: 
+                self.ui.videos_LW.addItem(i)
+        
     def keyPressEvent(self, e: QKeyEvent):
         if self.ui.upload_videos_text.hasFocus() and e.key() in (Qt.Key_Enter, Qt.Key_Return):
             self.upload_videos()
@@ -203,39 +279,45 @@ class MainWindow(QMainWindow):
     def on_home_btn_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(0)
         self.toggleShadow(self.ui.home_btn, self.ui.home_btn_shadow)
-    def on_show_data_btn_toggled(self):
+    def on_upload_images_btn_toggled(self):
+        self.update_list('images')
         self.ui.stackedWidget.setCurrentIndex(1)
-        self.toggleShadow(self.ui.show_data_btn, self.ui.show_data_btn_shadow)
-    def on_recg_face_btn_toggled(self):
+        self.toggleShadow(self.ui.upload_images_btn, self.ui.upload_images_btn_shadow)
+    def on_upload_videos_btn_toggled(self):
+        self.update_list('videos')
         self.ui.stackedWidget.setCurrentIndex(2)
-        self.toggleShadow(self.ui.recg_face_btn, self.ui.recg_face_btn_shadow)
-    def on_sett_panel_btn_toggled(self):
+        self.toggleShadow(self.ui.upload_videos_btn, self.ui.upload_videos_btn_shadow)
+    def on_show_data_btn_toggled(self):
+        self.show_data_list('images')
+        self.show_data_list('videos')
+        self.ui.profile_sec_lbl.hide()
+        self.ui.profile_sec.hide()
         self.ui.stackedWidget.setCurrentIndex(3)
-        self.toggleShadow(self.ui.sett_panel_btn, self.ui.sett_panel_btn_shadow)
+        self.toggleShadow(self.ui.show_data_btn, self.ui.show_data_btn_shadow)
     def on_add_camera_btn_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(4)
         self.toggleShadow(self.ui.add_camera_btn, self.ui.add_camera_btn_shadow)
-    def on_upload_videos_btn_toggled(self):
+    def on_sett_panel_btn_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(5)
-        self.toggleShadow(self.ui.upload_videos_btn, self.ui.upload_videos_btn_shadow)
-    def on_upload_images_btn_toggled(self):
+        self.toggleShadow(self.ui.sett_panel_btn, self.ui.sett_panel_btn_shadow)
+    def on_recg_face_btn_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(6)
-        self.toggleShadow(self.ui.upload_images_btn, self.ui.upload_images_btn_shadow)
+        self.toggleShadow(self.ui.recg_face_btn, self.ui.recg_face_btn_shadow)
     # -------------------------
     
-    # def closeEvent(self, event):
+    def closeEvent(self, event):
         
-    #     reply = QMessageBox.question(self, 'Message',
-    #         "It may take a while, are you sure to quit?", QMessageBox.Yes, QMessageBox.No)
+        reply = QMessageBox.question(self, 'Message',
+            "It may take a while, are you sure to quit?", QMessageBox.Yes, QMessageBox.No)
 
-    #     if reply == QMessageBox.Yes:
-    #         self.video_thread.requestInterruption()
-    #         self.recog_thread.requestInterruption()
-    #         self.video_thread.wait()
-    #         self.recog_thread.wait()
-    #         event.accept()
-    #     else:
-    #         event.ignore()
+        if reply == QMessageBox.Yes:
+            self.video_thread.requestInterruption()
+            self.recog_thread.requestInterruption()
+            self.video_thread.wait()
+            self.recog_thread.wait()
+            event.accept()
+        else:
+            event.ignore()
 
 
 if __name__ == "__main__":
